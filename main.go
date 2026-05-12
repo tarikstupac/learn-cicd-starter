98680fcbfc8601a677212ce930d41fcaccbbf9d6
Tarik Stupac <stupactarik@gmail.com>
2026-05-12 13:52:00 +0200

    fix gosec errors

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"embed"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +91,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	slog.Info("Serving on port: ", "port", port)
 	log.Fatal(srv.ListenAndServe())
 }
